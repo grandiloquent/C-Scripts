a457e58e25bad532704ffc787096a0a3098e1478
grandiloquent <1785907865@qq.com>
2023-03-06 11:14:02 +0800

    更新

--- 语音合成/Program.cs
+++ 语音合成/Program.cs
@@ -1,7 +1,45 @@
 
   using System.Net;
-using System.Text;
+  using System.Net.WebSockets;
+  using System.Text;
 using System.Text.Json;
+  using System.Text.RegularExpressions;
+  
+  async static Task GetAudio(ClientWebSocket connection, string fileName, string ttsText, string voice, string style)
+    {
+        var requestId = Guid.NewGuid().ToString("N").ToUpper();
+        var timestamp = DateTime.Now.ToUniversalTime().ToString("yyy-MM-dd'T'HH:mm:ss.fff'Z'");
+        await SendTextContent(connection, $@"Path: speech.config
+X-RequestId: {requestId}
+X-Timestamp: {timestamp}
+Content-Type: application/json
+
+{{""context"":{{""system"":{{""name"":""SpeechSDK"",""version"":""1.19.0"",""build"":""JavaScript"",""lang"":""JavaScript""}},""os"":{{""platform"":""Browser/Win32"",""name"":""Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"",""version"":""5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36""}}}}}}");
+
+        await SendTextContent(connection, $@"Path: synthesis.context
+X-RequestId: {requestId}
+X-Timestamp: {timestamp}
+Content-Type: application/json
+
+{{""synthesis"":{{""audio"":{{""metadataOptions"":{{""bookmarkEnabled"":false,""sentenceBoundaryEnabled"":false,""visemeEnabled"":false,""wordBoundaryEnabled"":false}},""outputFormat"":""audio-24khz-96kbitrate-mono-mp3""}},""language"":{{""autoDetection"":false}}}}}}");
+        //await SendSSML(connection, requestId, timestamp, ttsText);
+
+        await SendTextContent(connection,
+            // Xiaoxiao
+            // Yunxi
+            string.Format(@"Path: ssml
+X-RequestId: {0}
+X-Timestamp: {1}
+Content-Type: application/ssml+xml
+
+<speak xmlns=""http://www.w3.org/2001/10/synthesis"" xmlns:mstts=""http://www.w3.org/2001/mstts"" xmlns:emo=""http://www.w3.org/2009/10/emotionml"" version=""1.0"" xml:lang=""en-US""><voice name=""zh-CN-{3}Neural""><mstts:express-as style=""{4}"" ><prosody rate=""0%"" pitch=""0%"">{2}</prosody></mstts:express-as></voice></speak>"
+                , requestId, DateTime.Now.ToUniversalTime().ToString("yyy-MM-dd'T'HH:mm:ss.fff'Z'"), ttsText, voice,
+                style)
+        );
+        var data = await ReceiveAudio(connection);
+        File.WriteAllBytes(fileName, data);
+    }
+
   async static Task MsTts(string ttsText, string voice, string style)
     {
         var dir = $"{DateTime.Now:yyyyMMdd}".GetDesktopPath();
@@ -141,7 +179,18 @@ using System.Text.Json;
 
         return audioData.ToArray();
     }
-	
+
+ static void TimesXiaoxiao(string s)
+  {
+      MsTts(s, "Xiaoxiao", "newscast");
+  }
+
+ static void TimesYunfeng(string s)
+  {
+      MsTts(s, "Yunfeng", "newscast");
+  }
+  TimesYunfeng(ClipboardShare.GetText().Trim());
+
 /*
 dotnet publish -r win-x64 --self-contained false -o C:\Users\Administrator\Desktop\应用\应用 -p:PublishSingleFile=true,AssemblyName=语音合成
 */
