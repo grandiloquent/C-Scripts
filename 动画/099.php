d1e94f08dcf10146ec76b604a5903e35a3cf6768
grandiloquent <1785907865@qq.com>
2023-03-10 09:44:02 +0800

    更新

---
+++ 动画/099.php
@@ -0,0 +1,70 @@
+<?php
+$filepath = "C:/Program Files/Adobe/Adobe Photoshop 2023/Resources/IconResources.idx";
+$headerMagic = "Photoshop Icon Resource Index 1.0\n";
+
+$fp = fopen($filepath, "r");
+
+$magic = fread($fp, strlen($headerMagic));
+
+if ($magic != $headerMagic) {
+	exit("error: wrong magic");
+}
+
+// low res
+$dat = fgets($fp);
+echo $dat;
+
+// high res
+$dat = fgets($fp);
+echo $dat;
+
+// XLowRes : 80 extra bytes
+$dat = fgets($fp);
+echo $dat;
+
+// XHighRes : 80 extra bytes
+$dat = fgets($fp);
+echo $dat;
+
+$list = array();
+
+$entryLength = 368;
+
+function readOffsetFromEntry($entry, $position = 112) {
+	$name = substr($entry, 0, strpos($entry, "\0"));
+	$offset = substr($entry, $position, 4);
+	$hex = bin2hex($offset);
+	$offset = unpack("V", $offset);
+	$offset = array_pop($offset);
+	echo "$name : $offset 0x$hex\n";
+	return $offset;
+}
+
+while (!feof($fp)) {
+	$entry = fread ($fp, $entryLength);
+
+	if( strlen($entry) != $entryLength) {
+		break;
+	}
+
+	$name = substr($entry, 0, strpos($entry, "\0"));
+	$list[] = $name;
+
+	$offset = readOffsetFromEntry($entry, 144);
+
+	if ($offset == 0) {
+		continue;
+	}
+
+	$original = "C:/Users/Administrator/Desktop/应用/extracted/$offset.png";
+
+	// exit;
+	if (file_exists($original)) {
+		rename($original, "C:/Users/Administrator/Desktop/应用/extracted/$name.png");
+	}
+}
+
+
+
+
+?>
---
+++ 动画/100.php
@@ -0,0 +1,83 @@
+<?php
+$filepath = "C:/Program Files/Adobe/Adobe Photoshop 2023/Resources/PSIconsHighRes.dat";
+
+$fp = fopen($filepath, "r");
+
+$pngBuffer = "";
+$pngHeader = sprintf("%c%c%c%c%c%c%c%c", 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A);
+$pngHeaderLength = strlen($pngHeader);
+$count = 0;
+global $pngOffset;
+
+while (!feof($fp)) {
+	$char = fread($fp, 1);
+
+	// check for start of magic
+	if ($char != chr(0x89)) {
+		continue;
+	}
+	
+	// backtrack 1
+	fseek($fp, -1, SEEK_CUR);
+	printf("checking for full header\n");
+	// check for full header
+	$search = fread($fp, $pngHeaderLength);
+	if ($search == $pngHeader) {
+		fseek ($fp, 0 - $pngHeaderLength, SEEK_CUR);
+		$pngFile = readPNGFile($fp);
+	}
+	
+	// write out file
+	$pngfp = @fopen("C:/Users/Administrator/Desktop/应用/extracted/$pngOffset.png", "x");
+	@fwrite($pngfp, $pngFile);
+	@fclose($pngfp);
+	$count++;
+	
+}
+
+function readPNGFile($fp) {
+	global $pngHeader, $pngHeaderLength, $pngOffset;
+	$pos = ftell($fp);
+	$pngOffset = $pos;
+	echo "Reading PNG @ $pos\n";
+	
+	$contents = fread($fp, $pngHeaderLength);
+	
+	while ( ($chunk = readChunk($fp) ) ) {
+		$contents .= $chunk['data'];
+		
+		if ($chunk['name'] == "IEND") {
+			break;
+		}
+	}
+	
+	return $contents;
+}
+
+function readChunk($fp) {
+	$chunk = "";
+	
+	// Read length
+	$length = fread($fp, 4);
+	$chunk .= $length;
+	// turn length into int
+	$length = unpack("N", $length);
+	$length = array_pop($length);
+	
+	// Read chunk name
+	$name = fread($fp, 4);
+	$chunk .= $name;
+	printf("reading chunk $name with length %d\n", $length);
+	if ($length > 0) {
+		$chunk .= fread($fp, $length);
+	}
+	
+	$crc = fread($fp, 4);
+	$chunk .= $crc;
+	
+	return array("name" => $name, "data" => $chunk);
+}
+
+?>
+// https://gist.github.com/JohnCoates/ece63fa29abee3276a4c
+// C:\php\php.exe 100.php
